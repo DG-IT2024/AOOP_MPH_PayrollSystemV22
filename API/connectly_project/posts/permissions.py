eed71fbf726bc6ecc16210340ba3017501936ac0
DG-IT2024 <160723812+DG-IT2024@users.noreply.github.com>
2025-07-11 18:38:30 +0800

    Delete API directory

--- API/connectly_project/posts/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsTaskAssignee(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.assigned_to == request.user
-
